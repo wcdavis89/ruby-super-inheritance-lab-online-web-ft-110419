7600989336276be11f1b44b0ed654e6b94744f5a
Whittney Davis <wcdavis89@yahoo.com>
2019-12-03 23:22:43 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,2 +1,21 @@
 class ChattyStudent < Student
+    def hello
+    puts "Hey there! I.m so excite to learn stuff."
+  end
   
+  def raise_hand
+    puts "Pick me!"
+  end
+  
+  class user 
+  def log_in 
+    @logged_in = true 
+  end
+end
+
+class Student < User 
+  def log_in
+    super
+    @in_class = true 
+  end
+end
